e5e888c9b009fbfeefb4c0c85742d7521fdd9cab
Xin <zhangx0728@gmail.com>
2025-06-23 10:51:24 +0800

    Update bindings.lua

--- config/bindings.lua
+++ config/bindings.lua
@@ -56,7 +56,20 @@ local keys = {
    -- copy/paste --
    -- { key = 'c',          mods = 'CTRL',  action = act.CopyTo('Clipboard') },
    { key = 'v',          mods = 'CTRL',  action = act.PasteFrom('Clipboard') },
-   { key = 'c',          mods = 'CTRL|SHIFT',  action = act.CopyTo('Clipboard') },
+   {
+      key = 'c',
+      mods = 'CTRL',
+      action = wezterm.action_callback(function(window, pane)
+          selection_text = window:get_selection_text_for_pane(pane)
+          is_selection_active = string.len(selection_text) ~= 0
+          if is_selection_active then
+              window:perform_action(wezterm.action.CopyTo('ClipboardAndPrimarySelection'), pane)
+          else
+              window:perform_action(wezterm.action.SendKey{ key='c', mods='CTRL' }, pane)
+          end
+      end),
+  },
+   -- { key = 'c',          mods = 'CTRL|SHIFT',  action = act.CopyTo('Clipboard') },
    -- { key = 'v',          mods = 'CTRL|SHIFT',  action = act.PasteFrom('Clipboard') },
 
    -- tabs --
